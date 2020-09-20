8038f6a90c6108cd710ee27b14856c23b2869328
1565163445@qq.com <1565163445@qq.com>
2020-09-21 00:06:40 +0800

    继承

--- 2.写的都是看到的-面向对象编程/继承.js
+++ 2.写的都是看到的-面向对象编程/继承.js
@@ -1,21 +1,21 @@
 /*
  * @Author: your name
  * @Date: 2020-09-20 20:43:20
- * @LastEditTime: 2020-09-20 23:45:08
+ * @LastEditTime: 2020-09-20 23:48:35
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \JavaScript设计模式\2.写的都是看到的-面向对象编程\继承.js
  */
 // 类式继承
-console.log('类式继承');
+console.log("类式继承");
 // 父类
 function SuperClass() {
   this.superValue = true;
 }
 // 父类共有方法
-SuperClass.prototype.getSuperValue = function() {
+SuperClass.prototype.getSuperValue = function () {
   return this.superValue;
-}
+};
 // 子类
 function SubClass() {
   this.subValue = false;
@@ -23,9 +23,13 @@ function SubClass() {
 // 继承子类
 SubClass.prototype = new SuperClass();
 // 子类共有方法
-SubClass.prototype.getSubValue = function() {
+SubClass.prototype.getSubValue = function () {
   return this.subValue;
-}
+};
 var instance = new SubClass();
-console.log(instance.getSubValue());// false
-console.log(instance.getSuperValue());// true
+console.log(instance.getSubValue()); // false
+console.log(instance.getSuperValue()); // true
+// 测试
+console.log(instance instanceof SuperClass);// true
+console.log(instance instanceof SubClass);// true
+console.log(SubClass instanceof SuperClass);// false
