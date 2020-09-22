108ce7396c723825ddd0638a921cf01a38f7c9a7
1565163445@qq.com <1565163445@qq.com>
2020-09-22 15:33:31 +0800

    组合模式

--- 14.超值午餐-组合模式/1.组合要有容器类.js
+++ 14.超值午餐-组合模式/1.组合要有容器类.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-22 14:14:42
- * @LastEditTime: 2020-09-22 14:37:16
+ * @LastEditTime: 2020-09-22 15:12:18
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \JavaScript设计模式\14.超值午餐-组合模式\1.组合要有容器类.js
@@ -44,6 +44,12 @@ Container.prototype.init = function () {
 Container.prototype.getElement = function () {
   return this.element;
 };
+Container.prototype.add = function (info) {
+  this.children.push(info);
+  return this;
+};
 var container = new Container(1, "html");
 container.init();
+container.add("xkc").add("小卡车");
 console.log(container.getElement());
+console.log(container.children);
