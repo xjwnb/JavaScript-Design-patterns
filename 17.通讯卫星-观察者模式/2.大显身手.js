9a2a5f9ca70aa307807a584f7220be600dd34f28
1565163445@qq.com <1565163445@qq.com>
2020-09-23 15:54:36 +0800

    观察者模式

--- 17.通讯卫星-观察者模式/2.大显身手.js
+++ 17.通讯卫星-观察者模式/2.大显身手.js
@@ -1,11 +1,48 @@
 /*
  * @Author: your name
  * @Date: 2020-09-23 09:22:46
- * @LastEditTime: 2020-09-23 09:53:31
+ * @LastEditTime: 2020-09-23 15:54:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \JavaScript设计模式\17.通讯卫星-观察者模式\2.大显身手.js
  */
+// 观察者
+var Observer = (function () {
+  var _messages = {};
+  return {
+    // 注册信息接口
+    register: function (type, fn) {
+      if (typeof _messages[type] === "undefined") {
+        _messages[type] = [fn];
+      } else {
+        _messages[type].push(fn);
+      }
+    },
+    // 发布信息接口
+    fire: function (type, args) {
+      if (!_messages[type]) {
+        return;
+      }
+      // 定义消息信息
+      var events = {
+        type: type,
+        args: args || {},
+      };
+      for (var i = 0; i < _messages[type].length; i++) {
+        _messages[type][i].call(this, events);
+      }
+    },
+    // 移除信息接口
+    remove: function (type, fn) {
+      if (_messages[type] instanceof Array) {
+        for (var i = _messages[type].length - 1; i >= 0; i--) {
+          _messages[type][i] === fn && _messages[type].splice(i, 1);
+        }
+      }
+    },
+  };
+})();
+
 function $(id) {
   return document.getElementById(id);
 }
